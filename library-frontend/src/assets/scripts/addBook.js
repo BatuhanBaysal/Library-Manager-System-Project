c1b2c9ff95bb4ae4c4e1c410d6c709c1c5bc1568
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-10-15 12:35:33 +0300

    frontend-add book

---
+++ library-frontend/src/assets/scripts/addBook.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        const formData = {
+            title: document.getElementById('title').value,
+            isbn: document.getElementById('isbn').value,
+            author: document.getElementById('author').value,
+            publisher: document.getElementById('publisher').value,
+            yearOfPublication: document.getElementById('yearPublished').value,
+            placeOfPublication: document.getElementById('placePublished').value,
+            noOfAvailableCopies: document.getElementById('noOfAvailableCopies').value,
+            barcodeNumber: document.getElementById('barcodeNumber').value
+        }
+
+        addNewBook(formData, form);
+    });
+});
+
+function addNewBook(data, form) {
+    console.log('Attempting to add new book with data: ', data);
+
+    fetch('http://localhost:8080/api/books/addBook', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }) // API contract
+        .then(response => {
+            if (!response.ok) {
+                console.error('The response is not ok: ', response);
+                throw new Error('Failed to add new book');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Book added successfully: ', data);
+            alert('New book added successfully!');
+            form.reset();
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+            alert('Failed to add new book. Please try again.');
+        });
+}
